0be86994d09637eb2630d29b530ecca83c65e848
Marc-Antoine Lortie <marc-antoine.lortie@outlook.com>
2016-01-25 10:38:58 -0500

    Commit "Night at the office" source files.
    Fixed:
    
    - "Could not load library client." when launching the mod.
    -  liblist.gam now has correct parameters.
    
    Changes:
    
    - Dropped use of FMOD library in favor of Steam Half-Life's built
    in MP3 player, due to many problems related to FMOD.
    
    Players can hear music and the music will loop properly.
    Unfortunately, MP3 songs stop right after level change.

---
+++ noffice/dlls/wpn_shared/hl_wpn_glock.cpp
@@ -0,0 +1,359 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "nodes.h"
+#include "player.h"
+
+enum glock_e {
+	GLOCK_IDLE1 = 0,
+	GLOCK_IDLE2,
+	GLOCK_IDLE3,
+	GLOCK_SHOOT,
+	GLOCK_SHOOT_EMPTY,
+	GLOCK_RELOAD,
+	GLOCK_RELOAD_NOT_EMPTY,
+	GLOCK_DRAW,
+	GLOCK_HOLSTER,
+	GLOCK_ADD_SILENCER
+};
+
+LINK_ENTITY_TO_CLASS( weapon_glock, CGlock );
+LINK_ENTITY_TO_CLASS( weapon_9mmhandgun, CGlock );
+
+
+void CGlock::Spawn( )
+{
+	pev->classname = MAKE_STRING("weapon_9mmhandgun"); // hack to allow for old names
+	Precache( );
+	m_iId = WEAPON_GLOCK;
+	SET_MODEL(ENT(pev), "models/w_9mmhandgun.mdl");
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	SetClipModel("models/w_9mmclip.mdl");
+
+	// ==========================================
+	// Code changes for- Night at the Office:
+	// ==========================================
+	//
+	// -Randomised Ammo. Picking up a gun from a fallen terrorist 
+	//  will not give you a pre-defined amount of bullets. The exact 
+	//  number is random (depending on the gun and clip size), which 
+	//  means the player will constantly need to keep a check on the 
+	//  ammo as it will no longer be 'comfortable' for the player to 
+	//  waste ammo.
+
+	m_iDefaultAmmo = DefaultAmmoBySkill(GLOCK_MAX_CLIP, gSkillData.iSkillLevel);
+#else
+	m_iDefaultAmmo = GLOCK_DEFAULT_GIVE;
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+
+	FallInit();// get ready to fall down.
+}
+
+
+void CGlock::Precache( void )
+{
+	PRECACHE_MODEL("models/v_9mmhandgun.mdl");
+	PRECACHE_MODEL("models/w_9mmhandgun.mdl");
+	PRECACHE_MODEL("models/p_9mmhandgun.mdl");
+
+	m_iShell = PRECACHE_MODEL ("models/shell.mdl");// brass shell
+
+	PRECACHE_SOUND("items/9mmclip1.wav");
+	PRECACHE_SOUND("items/9mmclip2.wav");
+
+	PRECACHE_SOUND ("weapons/pl_gun1.wav");//silenced handgun
+	PRECACHE_SOUND ("weapons/pl_gun2.wav");//silenced handgun
+	PRECACHE_SOUND ("weapons/pl_gun3.wav");//handgun
+
+	m_usFireGlock1 = PRECACHE_EVENT( 1, "events/glock1.sc" );
+	m_usFireGlock2 = PRECACHE_EVENT( 1, "events/glock2.sc" );
+}
+
+int CGlock::GetItemInfo(ItemInfo *p)
+{
+	p->pszName = STRING(pev->classname);
+	p->pszAmmo1 = "9mm";
+	p->iMaxAmmo1 = _9MM_MAX_CARRY;
+	p->pszAmmo2 = NULL;
+	p->iMaxAmmo2 = -1;
+	p->iMaxClip = GLOCK_MAX_CLIP;
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	p->iSlot = 0;
+	p->iPosition = 3;
+#else
+	p->iSlot = 1;
+	p->iPosition = 0;
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	p->iFlags = 0;
+	p->iId = m_iId = WEAPON_GLOCK;
+	p->iWeight = GLOCK_WEIGHT;
+
+	return 1;
+}
+
+BOOL CGlock::Deploy( )
+{
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	BOOL bResult = DefaultDeploy("models/v_9mmhandgun.mdl", "models/p_9mmhandgun.mdl", GLOCK_DRAW, "onehanded", /*UseDecrement() ? 1 : 0*/ 0);
+
+	if (bResult)
+	{
+		m_fInAttack = 0;
+	}
+
+	return bResult;
+#else
+	// pev->body = 1;
+	return DefaultDeploy( "models/v_9mmhandgun.mdl", "models/p_9mmhandgun.mdl", GLOCK_DRAW, "onehanded", /*UseDecrement() ? 1 : 0*/ 0 );
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+}
+
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+void CGlock::Holster(int skiplocal /*= 0*/)
+{
+	m_fInReload = FALSE;// cancel any reload in progress.
+
+	m_pPlayer->m_flNextAttack = UTIL_WeaponTimeBase() + 0.5;
+	SendWeaponAnim(GLOCK_HOLSTER);
+
+	m_fInAttack = 0;
+}
+#endif // defined ( EFTD_DLL ) || defined ( EFTD_CLIENT_DLL )
+
+void CGlock::SecondaryAttack( void )
+{
+#if !defined ( NOFFICE_DLL ) && !defined ( NOFFICE_CLIENT_DLL )
+	GlockFire( 0.1, 0.2, FALSE );
+#endif // !defined ( NOFFICE_DLL ) && !defined ( NOFFICE_CLIENT_DLL )
+}
+
+void CGlock::PrimaryAttack( void )
+{
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	GlockFire( 0.01, 0.2, TRUE );
+#else
+	GlockFire( 0.01, 0.3, TRUE );
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+}
+
+void CGlock::GlockFire( float flSpread , float flCycleTime, BOOL fUseAutoAim )
+{
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	// Do not allow attack unless primary attack key was released.
+	if (m_fInAttack)
+		return;
+
+	if (m_iClip <= 0)
+	{
+		if (!m_fFireOnEmpty)
+			Reload( );
+		else
+		{
+			EMIT_SOUND(ENT(m_pPlayer->pev), CHAN_WEAPON, "weapons/357_cock1.wav", 0.8, ATTN_NORM);
+			m_flNextPrimaryAttack = GetNextAttackDelay(0.2);
+		}
+
+		return;
+	}
+
+	// Prevent from continuously refire.
+	m_fInAttack = 1;
+#else
+	if (m_iClip <= 0)
+	{
+		if (m_fFireOnEmpty)
+		{
+			PlayEmptySound();
+			m_flNextPrimaryAttack = GetNextAttackDelay(0.2);
+		}
+
+		return;
+	}
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+
+	m_iClip--;
+
+	m_pPlayer->pev->effects = (int)(m_pPlayer->pev->effects) | EF_MUZZLEFLASH;
+
+	int flags;
+
+#if defined( CLIENT_WEAPONS )
+	flags = FEV_NOTHOST;
+#else
+	flags = 0;
+#endif
+
+	// player "shoot" animation
+	m_pPlayer->SetAnimation( PLAYER_ATTACK1 );
+
+	// silenced
+	if (pev->body == 1)
+	{
+		m_pPlayer->m_iWeaponVolume = QUIET_GUN_VOLUME;
+		m_pPlayer->m_iWeaponFlash = DIM_GUN_FLASH;
+	}
+	else
+	{
+		// non-silenced
+		m_pPlayer->m_iWeaponVolume = NORMAL_GUN_VOLUME;
+		m_pPlayer->m_iWeaponFlash = NORMAL_GUN_FLASH;
+	}
+
+	Vector vecSrc	 = m_pPlayer->GetGunPosition( );
+	Vector vecAiming;
+	
+	if ( fUseAutoAim )
+	{
+		vecAiming = m_pPlayer->GetAutoaimVector( AUTOAIM_10DEGREES );
+	}
+	else
+	{
+		vecAiming = gpGlobals->v_forward;
+	}
+
+	Vector vecDir;
+	vecDir = m_pPlayer->FireBulletsPlayer( 1, vecSrc, vecAiming, Vector( flSpread, flSpread, flSpread ), 8192, BULLET_PLAYER_9MM, 0, 0, m_pPlayer->pev, m_pPlayer->random_seed );
+
+	PLAYBACK_EVENT_FULL( flags, m_pPlayer->edict(), fUseAutoAim ? m_usFireGlock1 : m_usFireGlock2, 0.0, (float *)&g_vecZero, (float *)&g_vecZero, vecDir.x, vecDir.y, 0, 0, ( m_iClip == 0 ) ? 1 : 0, 0 );
+
+	m_flNextPrimaryAttack = m_flNextSecondaryAttack = GetNextAttackDelay(flCycleTime);
+
+	if (!m_iClip && m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0)
+		// HEV suit - indicate out of ammo condition
+		m_pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
+
+	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat( m_pPlayer->random_seed, 10, 15 );
+}
+
+
+void CGlock::Reload( void )
+{
+	if ( m_pPlayer->ammo_9mm <= 0 )
+		 return;
+
+	int iResult;
+
+	if (m_iClip == 0)
+		iResult = DefaultReload( 17, GLOCK_RELOAD, 1.5 );
+	else
+		iResult = DefaultReload( 17, GLOCK_RELOAD_NOT_EMPTY, 1.5 );
+
+	if (iResult)
+	{
+		m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat( m_pPlayer->random_seed, 10, 15 );
+
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+		// Unblock primary attack.
+		m_fInAttack = 0;
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	}
+}
+
+
+
+void CGlock::WeaponIdle( void )
+{
+	ResetEmptySound( );
+
+	m_pPlayer->GetAutoaimVector( AUTOAIM_10DEGREES );
+
+#if defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+	//
+	// Unblock primary attack.
+	// This will only occur if players released primary attack key.
+	//
+	m_fInAttack = 0;
+#endif // defined ( NOFFICE_DLL ) || defined ( NOFFICE_CLIENT_DLL )
+
+	if ( m_flTimeWeaponIdle > UTIL_WeaponTimeBase() )
+		return;
+
+	// only idle if the slid isn't back
+	if (m_iClip != 0)
+	{
+		int iAnim;
+		float flRand = UTIL_SharedRandomFloat( m_pPlayer->random_seed, 0.0, 1.0 );
+
+		if (flRand <= 0.3 + 0 * 0.75)
+		{
+			iAnim = GLOCK_IDLE3;
+			m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 49.0 / 16;
+		}
+		else if (flRand <= 0.6 + 0 * 0.875)
+		{
+			iAnim = GLOCK_IDLE1;
+			m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 60.0 / 16.0;
+		}
+		else
+		{
+			iAnim = GLOCK_IDLE2;
+			m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 40.0 / 16.0;
+		}
+		SendWeaponAnim( iAnim, 1 );
+	}
+}
+
+
+
+
+
+
+
+
+class CGlockAmmo : public CBasePlayerAmmo
+{
+	void Spawn( void )
+	{ 
+		Precache( );
+		SET_MODEL(ENT(pev), "models/w_9mmclip.mdl");
+		CBasePlayerAmmo::Spawn( );
+	}
+	void Precache( void )
+	{
+		PRECACHE_MODEL ("models/w_9mmclip.mdl");
+		PRECACHE_SOUND("items/9mmclip1.wav");
+	}
+	BOOL AddAmmo( CBaseEntity *pOther ) 
+	{ 
+		if (pOther->GiveAmmo( AMMO_GLOCKCLIP_GIVE, "9mm", _9MM_MAX_CARRY ) != -1)
+		{
+			EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/9mmclip1.wav", 1, ATTN_NORM);
+			return TRUE;
+		}
+		return FALSE;
+	}
+};
+LINK_ENTITY_TO_CLASS( ammo_glockclip, CGlockAmmo );
+LINK_ENTITY_TO_CLASS( ammo_9mmclip, CGlockAmmo );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
